f4f8752a03b50bbc1867b8dc7eb6bdf524018985
hgrzybowski <h-grzybowski@wp.pl>
2019-05-10 18:14:24 +0200

    newControllers

--- FileRepository/Controllers/FileController.cs
+++ FileRepository/Controllers/FileController.cs
@@ -5,12 +5,16 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
+using FileRepository.Abstract;
 using System.Web.Mvc;
+using FileRepository.Helpers;
 
 namespace FileRepository.Controllers
 {
     public class FileController : Controller
     {
+        IFileHelper zaimplementowany_interfejs = new FileHelper();
+
         // GET: File
         public ActionResult Index()
         {
@@ -35,6 +39,29 @@ namespace FileRepository.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult Delete(string path)
+        {
 
+            //Debug.WriteLine(path);
+            string moja_sciezka_do_tego_projektu = AppDomain.CurrentDomain.BaseDirectory;
+            string pelna_sciezka_do_moich_plikow = moja_sciezka_do_tego_projektu + "App_Data/Upload/";
+            string sciezka = pelna_sciezka_do_moich_plikow + path;
+            //Debug.WriteLine(sciezka);
+            zaimplementowany_interfejs.Delete(sciezka);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Download(string filename)
+        {
+
+            string moja_sciezka_do_tego_projektu = AppDomain.CurrentDomain.BaseDirectory;
+            string pelna_sciezka_do_moich_plikow = moja_sciezka_do_tego_projektu + "App_Data/Upload/";
+            string sciezka = pelna_sciezka_do_moich_plikow + filename;
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(sciezka);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filename);
+
+        }
     }
 }
---
+++ FileRepository/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FileRepository.Models;
+using System.Web.Mvc;
+
+namespace FileRepository.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Register register)
+        {
+            return Content(string.Format($"Witaj drogi : {register.Name} {register.Surname}"));
+        }
+
+
+    }
+}
