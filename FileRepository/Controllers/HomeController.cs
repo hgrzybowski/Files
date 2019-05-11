e244ff78f55b56a7c32b53c0a7ba0c3111154c37
hgrzybowski <h-grzybowski@wp.pl>
2019-05-11 12:05:59 +0200

    addFiles

--- FileRepository/Controllers/HomeController.cs
+++ FileRepository/Controllers/HomeController.cs
@@ -5,24 +5,18 @@ namespace FileRepository.Controllers
 {
     public class HomeController : Controller
     {
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Register()
-        {
-
-            return View();
-        }
         [HttpPost]
         public ActionResult Register(Register register)
         {
-            return Content(string.Format($"Witaj drogi : {register.Name} {register.Surname}"));
+          
+            return View();
         }
-
-
     }
 }
